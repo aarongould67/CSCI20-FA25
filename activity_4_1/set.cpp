1b5601cff5df7aeac35cfe6d66fb4e5b0c81baff
aarongould67 <agould002@student.butte.edu>
2025-09-23 20:37:43 +0000

    w4 act 1

--- activity_4_1/set.cpp
+++ activity_4_1/set.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+#include <cstdio> // includes printf library
+#include <iostream> //
+#include <array> // includes array library
+#include <string>
 
 int main() {
+
+    std::array<int, 5> car = {1,2,3,4,5};
+    
+    std::array<char, 8>  boat = {'h','i', ' ','t','h','e','r','e'};
+    
+    std::array<std::string, 2> squid_str = {"hi", "there"};
+    
+    std::printf("%i\n" ,car.at(1));
+    
+    std::printf("%c\n", boat.at(1));
+    
+    std::printf("%s\n", squid_str.at(0).c_str());
+    
     return 0;
     
 }
